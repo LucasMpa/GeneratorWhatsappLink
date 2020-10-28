0a43967ea94439a5d2b10715e1104dcb94ae733c
Lucas <lucas.mpap@gmail.com>
2020-10-28 10:51:13 -0300

    Updates

---
+++ precache-manifest.2d383fe981af514a404a3604059333aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1896efeb30107d5972f61dfe297ed07a",
+    "url": "/GeneratorWhatsappLink/index.html"
+  },
+  {
+    "revision": "5ec072bbe56b5284e7a0",
+    "url": "/GeneratorWhatsappLink/static/css/main.7519b287.chunk.css"
+  },
+  {
+    "revision": "5c48a7807618b4eb6b0a",
+    "url": "/GeneratorWhatsappLink/static/js/2.c1368c58.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/GeneratorWhatsappLink/static/js/2.c1368c58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ec072bbe56b5284e7a0",
+    "url": "/GeneratorWhatsappLink/static/js/main.37e299db.chunk.js"
+  },
+  {
+    "revision": "7e4a4faca5c27ef3a817",
+    "url": "/GeneratorWhatsappLink/static/js/runtime-main.106327a1.js"
+  }
+]);
