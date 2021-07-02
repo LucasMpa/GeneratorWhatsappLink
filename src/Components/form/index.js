a25681a9e45df53ae5d9aca93c67f2c29d756347
Lucas Matheus <lucasmpa16@gmail.com>
2021-07-02 08:11:51 -0300

    Implements copy and clue funcionality

--- src/Components/form/index.js
+++ src/Components/form/index.js
@@ -1,127 +1,189 @@
 import React, { useState } from "react";
-import './styles.css'
-import InputPhoneMask from "../inputPhoneMask";
-import InputMask from 'react-input-mask';
-import Helper from '../../utils/Helper';
+import "./styles.css";
+import InputMask from "react-input-mask";
+import Helper from "../../utils/Helper";
+import copy from "copy-to-clipboard";
 
 export default function FormPage() {
+  const [phoneNumber, setphoneNumber] = useState("");
+  const [msgText, setMsgText] = useState("");
+  const [data, setData] = useState("");
+  const [successAlert, setSuccesAlert] = useState(false);
+  const [dangerAlert, setDangerAlert] = useState(false);
 
-
-    const [phoneNumber, setphoneNumber] = useState("");
-    const [msgText, setMsgText] = useState("");
-    const [data, setData] = useState("");
-    const [successAlert, setSuccesAlert] = useState(false);
-    const [dangerAlert, setDangerAlert] = useState(false);
-    
-
-    function Api() {
-        if(phoneNumber !== '' && msgText !== '') {
-            setData(`https://api.whatsapp.com/send?phone=55${Helper.onlyNumbers(phoneNumber)}&text=${msgText}`)
-        
-        }
-       if(phoneNumber.length >= 14 && msgText !== ''){
-            setSuccesAlert(true)
-       } else {
-        setDangerAlert(true)
-          
-       }
+  function Api() {
+    if (phoneNumber !== "" && msgText !== "") {
+      setData(
+        `https://api.whatsapp.com/send?phone=55${Helper.onlyNumbers(
+          phoneNumber
+        )}&text=${msgText}`
+      );
     }
 
-    function clearValues(){
-        setDangerAlert('')
-        setSuccesAlert('')
+    if (phoneNumber.length >= 14 && msgText !== "") {
+      setSuccesAlert(true);
+      setDangerAlert(false);
+      setTimeout(() => setSuccesAlert(null), 3000);
+    } else {
+      setDangerAlert(true);
+      setSuccesAlert(false);
+      setTimeout(() => setDangerAlert(null), 3000);
     }
-console.log(dangerAlert)
+  }
 
-    return(
-        
-        <div className="main">
-            <div className="form-container">
-                {
-                    dangerAlert  ?   <div class="alert alert-danger" role="alert"> This is a danger alert—check it out!
-                </div>  : null
+  function clearValues() {
+    setDangerAlert(null);
+    setSuccesAlert(null);
+  }
+  // data != "" && copy(data);
 
-                }
-                {
-                    successAlert ? <div class="alert alert-success" role="alert"> This is a success alert—check it out!
-                </div> : null
-                }
-                
-               
-                    <div className="form-group">
-                        <h1>Gerador de Link para Whatsaap</h1>
-                        <label>
-                            Número de celular
-                        </label>
-                       
-                        <InputMask 
-                            onChange={(e) => {setphoneNumber(e.target.value)}}
-                            type="tel" mask="(99) 9 9999-9999"
-                            maskChar={null}
-                            placeholder="(99) 9 9999-9999"
-                            className="form-control"
-                            onClick={() => {clearValues()}}
-                            />
-                            
-                        <label>Mensagem</label>
-                        <form>
-                            <textarea 
-                            onKeyUp={(e) => { setDangerAlert(e.target.value)
-                                
-                            }}
-                            className="form-control" 
-                            id=""
-                            placeholder="Digite seu texto..." rows="5"  
-                            onChange={(e) => {setMsgText(e.target.value)}}
-                            />  
-                        </form>
-                        <small className="form-text text-muted">Não guardamos nenhum dado informado.</small>
-                    </div>
-                    <div className="btn-copy">
-                            <button
-                            onClick={Api}
-                            type="button" 
-                            className="btn btn-success"
-                            >
-                                Gerar Link
-                            </button>
-                            <small>
-                                <a href={data}>{data}</a>
-                            </small>
-                            
-                            
-                    </div>
+  return (
+    <div className="main">
+      <div className="form-container">
+        {dangerAlert && (
+          <div class="alert alert-danger" role="alert">
+            Erro ao gerar link.
+          </div>
+        )}
+        {successAlert && (
+          <div class="alert alert-success" role="alert">
+            {" "}
+            Sucesso ao gerar o link.
+          </div>
+        )}
+        {!data && (
+          <div className="form-group">
+            <h1>Gerador de Link para Whatsaap</h1>
+            <label>Número de celular</label>
 
-                    <div className="side-content">
-                        <h2>Como Funciona?</h2>
-                        <ul>
-                            <li>Insira o nº do WhatsApp Ex: 85 9 9660-5866</li> 
-                            <li>Escreva a mensagem padrão que será exibida</li> 
-                            <li>Clique em “<strong>GERAR LINK</strong>”</li> 
-                            <li>Copie o link e compartilhe</li> 
-                            <li>Antes de usar, faça o msgText</li>
-                        </ul>
-                        
-                        <div>
-                            <small className="form-text text-muted">
-                            <pre>{data}</pre>
-                                O gerador de links é uma ótima ferramenta para ações de marketing ou relacionamento. 
-                                Com o link para mensagens personalizadas, você poderá utilizar em campanhas, redes sociais, email marketing, banners e etc. 
-                                O bom de encurtar e personalizar links e mensagens do WhatsApp é que funcionará no desktop e no mobile da mesma forma. 
-                                Faça bom uso da ferramenta encurtadora de WhatsApp.
-                            </small>    
-                        </div>
-                    </div>
+            <InputMask
+              onChange={(e) => {
+                setphoneNumber(e.target.value);
+              }}
+              type="tel"
+              mask="(99) 9 9999-9999"
+              maskChar={null}
+              placeholder="(99) 9 9999-9999"
+              className="form-control"
+              onClick={() => {
+                clearValues();
+              }}
+            />
+
+            <label>Mensagem</label>
+            <form>
+              <textarea
+                className="form-control"
+                placeholder="Digite seu texto..."
+                rows="5"
+                onChange={(e) => {
+                  setMsgText(e.target.value);
+                }}
+                onClick={() => {
+                  clearValues();
+                }}
+              />
+            </form>
+            <small className="form-text text-muted">
+              Não guardamos nenhum dado informado.
+            </small>
+
+            <div className="btn-copy">
+              <button
+                onClick={() => Api()}
+                type="button"
+                className="btn btn-success"
+              >
+                Gerar Link
+              </button>
+              <small>
+                <a href={data}>{data}</a>
+              </small>
             </div>
-            <div className="publicity-container">
-                <title-pub>
-                
-                    Em construção
-                </title-pub>
-            </div>  
+          </div>
+        )}
+
+        {data && (
+          <div
+            style={{
+              display: "flex",
+              width: "100%",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "column",
+              padding: "30px",
+            }}
+          >
+            
+              Link Gerado:
+              <a href={data} target="_blank">
+                {data}
+              </a>{" "}
+           
+            <br />
+            <h1 style={{ textAlign: "center" }}>Pronto! </h1>
+            <h6 style={{ textAlign: "center" }}>
+              Copie e compartilhe com usuários do Whatsapp
+            </h6>
+
+            <br />
+            <div
+              style={{
+                width: "50%",
+                display: "flex",
+                justifyContent: "space-evenly",
+              }}
+            >
+              <button
+                onClick={() => {
+                  copy(data);
+                }}
+                className="btn btn-success"
+              >
+                Copiar
+              </button>
+
+              <button
+                onClick={() => {
+                  setData("");
+                  clearValues();
+                }}
+                type="button"
+                className="btn btn-success"
+              >
+                Gerar Novo Link
+              </button>
+            </div>
+          </div>
+        )}
+
+        <div className="side-content">
+          <h2>Como Funciona?</h2>
+          <ul>
+            <li>Insira o nº do WhatsApp Ex: 85 9 9660-5866</li>
+            <li>Escreva a mensagem padrão que será exibida</li>
+            <li>
+              Clique em “<strong>GERAR LINK</strong>”
+            </li>
+            <li>Copie o link e compartilhe</li>
+            <li>Antes de usar, faça o msgText</li>
+          </ul>
+
+          <div>
+            <small className="form-text text-muted">
+              O gerador de links é uma ótima ferramenta para ações de marketing
+              ou relacionamento. Com o link para mensagens personalizadas, você
+              poderá utilizar em campanhas, redes sociais, email marketing,
+              banners e etc. O bom de encurtar e personalizar links e mensagens
+              do WhatsApp é que funcionará no desktop e no mobile da mesma
+              forma. Faça bom uso da ferramenta encurtadora de WhatsApp.
+            </small>
+          </div>
         </div>
-             
-    );
+      </div>
+      <div className="publicity-container">
+        <title-pub>Em construção</title-pub>
+      </div>
+    </div>
+  );
 }
-
-
